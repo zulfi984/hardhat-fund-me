246780f38292b26d4227dfabbc6ebabd4b096532
zulfi984 <zulfizee1@gmail.com>
2022-07-24 13:35:28 +0530

    first commit

---
+++ contracts/test/MockV3Aggregator.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+import "@chainlink/contracts/src/v0.6/tests/MockV3Aggregator.sol"; // taking copy of local aggregatorpricefeed
+// mock is called taking copy of real objects to run locally
---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,27 @@
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { getNamedAccounts, deployments, network } = require("hardhat")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    let ethUsdPriceFeedAddress
+    if (chainId == 31337) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+    log("----------------------------------------------------")
+    log("Deploying FundMe and waiting for confirmations...")
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress],
+        log: true,
+        // we need to wait if on a live network so we can verify properly
+    })
+}
+
+module.exports.tags = ["all", "fundme"]
