cd33525424dfc15709fb76b746b72f2d3308e987
zulfi984 <zulfizee1@gmail.com>
2022-07-24 17:52:23 +0530

    secnd

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "./PriceConverter.sol";
 
-error NotOwner();
+error FundMe__NotOwner();
 
 contract FundMe {
     using PriceConverter for uint256;
@@ -37,7 +37,7 @@ contract FundMe {
     
     modifier onlyOwner {
         // require(msg.sender == owner);
-        if (msg.sender != i_owner) revert NotOwner();
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
         _;
     }
     
--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,5 +1,6 @@
-const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { getNamedAccounts, deployments, network } = require("hardhat")
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
@@ -15,13 +16,21 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     }
     log("----------------------------------------------------")
     log("Deploying FundMe and waiting for confirmations...")
-    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     const fundMe = await deploy("FundMe", {
         from: deployer,
         args: [ethUsdPriceFeedAddress],
         log: true,
         // we need to wait if on a live network so we can verify properly
+        waitConfirmations: network.config.blockConfirmations || 1,
     })
+    log(`FundMe deployed at ${fundMe.address}`)
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress])
+    }
 }
 
 module.exports.tags = ["all", "fundme"]
